bea542ae48c379c03e122b0d0350625656c4fc1d
mokautsar30 <mohammadkautsar30@gmail.com>
2024-06-08 19:14:53 +0700

    feat: project-controller-done

--- routes/project.js
+++ routes/project.js
@@ -5,6 +5,8 @@ const router = require("express").Router();
 
 router.post('/', ProjectController.createProject)
 router.get('/', ProjectController.viewProject)
+router.put('/:id', ProjectController.editProjectById)
+router.delete('/:id', ProjectController.deleteProjectById)
 
 
 module.exports = router;
